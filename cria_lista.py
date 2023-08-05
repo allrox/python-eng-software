7a32710c158319ad59fa1ccbd064e798f7297023
allrox <alexandre@allrox.com.br>
2023-08-05 16:11:47 -0300

    Correção da função anterior, construída com FOR

--- cria_lista.py
+++ cria_lista.py
@@ -1,8 +1,11 @@
 # Função para criar lista com x elementos e incremento y
-def cria_lista(itens, intervalo):
+def cria_lista(elemento, fim, intervalo):
+    # Definindo a variável lista
     lista = []
-    for i in range(itens):
-        i += intervalo
-        lista.append(i)
-    return lista
 
+    # Preenchendo a lista com um laço while
+    while elemento <= fim:
+        lista.append(elemento)
+        elemento += intervalo
+
+    return lista
