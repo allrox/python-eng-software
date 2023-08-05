c8968abebf52747ebf0bbbaaf6ddef64e3bd0ba4
allrox <alexandre@allrox.com.br>
2023-08-05 16:29:13 -0300

    Correções e otimização

--- fatorial.py
+++ fatorial.py
@@ -2,7 +2,7 @@ from linha_separadora import linha_separadora
 
 
 # Função para calcular o fatorial n! = n(n-1)
-# Método recursivo
+# Método recursivo (O método chama a si mesmo: Linha 12)
 def funcao_fatorial(num):
 
     # Avalia se o número informado é 0 ou 1, valores cujo fatorial é 1
@@ -18,16 +18,33 @@ print(f"O fatorial do número {n} é: {funcao_fatorial(n)}")
 linha_separadora("-")
 
 
-# Método iterativo
+# Método iterativo (Trabalha através das iterações)
 def funcao_fatorial_iterativo(num):
     f = 1
 
     # Observe que, neste caso, o cálculo usa o range invertido, de 1 até num + 1,
-    # encerrando o laço quando num atinge o parâmetro da função
+    # encerrando o laço quando num atinge o parâmetro da função.
+    # "num + 1" é utilizado pois o FOR não consideraria o último item da lista (exceto).
 
     for i in range(1, num + 1):
         f = f * i
     return f
 
 
-print(f"O fatorial é: {funcao_fatorial_iterativo(4)}")
+n = 4
+print(f"O fatorial de {n} é {funcao_fatorial_iterativo(n)}.")
+
+linha_separadora("-")
+
+def fatorial(n):
+    fat = 1
+    if n == 0 or n == 1:
+        return fat
+    else:
+        for x in range(2, n + 1):
+               fat = fat*x
+        return fat
+
+
+n = 5
+print(f"O fatorial de {n} é {fatorial(n)}")
