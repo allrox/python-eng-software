b66bd0166fb82a2886cd8980b910c680e0e9a803
allrox <alexandre@allrox.com.br>
2023-08-07 18:42:42 -0300

    Implementado tratamento de exceções

--- comparador.py
+++ comparador.py
@@ -17,20 +17,32 @@ def comparar(x, y):
 
 # Comparando os números através da função
 comparar(8, 89)
+comparar(75, 32)
 
 linha_separadora(".")
 
 
 # Função para indicar o número ímpar
 def par_ou_impar(x):
-    if (x % 2) != 0:
-        print(f"O número {x} é ímpar.")
-    else:
-        print(f"O número {x} é par.")
+
+    # Implementando o tratamento de exceções
+    while True:
+        try:
+            x = int(x)
+            if (x % 2) != 0:
+                print(f"O número {x} é ímpar.")
+            else:
+                print(f"O número {x} é par.")
+            break
+        except ValueError:
+            print("Erro: Não foi informado um número válido! ")
+            break
 
 
 # Avaliando um número através da função
-par_ou_impar(100)
+par_ou_impar(13)
+par_ou_impar("n")
+par_ou_impar(8)
 
 
 linha_separadora(".")
@@ -38,13 +50,26 @@ linha_separadora(".")
 
 # Função para avaliar a média de um aluno
 def avaliar_media(x):
-    if x < 5:
-        print(f"A média {x} indica que o aluno está reprovado.")
-    elif x < 7:
-        print(f"A média {x} indica que o aluno está em recuperação.")
-    else:
-        print(f"A média {x} indica que o aluno está aprovado.")
+
+    # Tratamento de exceção para formato de entrada
+    try:
+
+        # Verifica se a nota média informada está na faixa válida 0 - 10
+        if (x >= 0) and (x <= 10):
+            if x < 5:
+                print(f"A média {x} indica que o aluno está reprovado.")
+            elif x < 7:
+                print(f"A média {x} indica que o aluno está em recuperação.")
+            elif x <= 10:
+                print(f"A média {x} indica que o aluno está aprovado.")
+        else:
+            print(input("Informe uma média entre 0 e 10"))
+
+    # Resposta para a exceção
+    except TypeError:
+        print("Erro: Entrada de dados incompatível!")
 
 
 # Avaliando a média através da função
-avaliar_media(6.4)
+x = 8
+avaliar_media(x)
